505620edad0cba3fb4f9240825a90824c841e352
Halsey Filbin <halsey.t.filbin@gmail.com>
2023-10-08 15:15:15 -0500

    armstrong numbers solved

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,18 @@
 # How can you make this more scalable and reusable later?
+def is_armstrong_number(number):
+    if number < 10:
+        return True
+    digits = [int(num) for num in str(number)]
+    power =len(digits)
+    total = 0
+    for digit in digits:
+        total += digit ** power
+    return total == number
+
 def find_armstrong_numbers(numbers):
+    armstrong_numbers = []
+    for num in numbers:
+        if is_armstrong_number(num):
+            armstrong_numbers.append(num)
+    return armstrong_numbers
+
--- python/armstrong_numbers_spec.py
+++ python/armstrong_numbers_spec.py
@@ -1,6 +1,19 @@
 from armstrong_numbers import find_armstrong_numbers
 
-print(find_armstrong_numbers([0]) == [0]) 
-print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
-print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+print(find_armstrong_numbers([0])) 
+print(find_armstrong_numbers(list(range(0, 8))))
+print(find_armstrong_numbers(list(range(0,100))))
+print(find_armstrong_numbers(list(range(0,999))))
+
+def test_find_armstrong_numers_0():
+    assert find_armstrong_numbers([0]) == 0
+
+def test_find_armstrong_numers_small():
+    assert find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7]
+
+def test_find_armstrong_numers_medium():
+    assert find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def test_find_armstrong_numers_large():
+    assert find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
+
